c10f3f9e0da76f824f92abd028b521ce1aab4ae8
Voskesss <josklijnhout@hotmail.com>
2025-01-24 13:10:43 +0100

    video

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,53 +1,25 @@
-import { useTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 
 const SEO = () => {
-  const { t, i18n } = useTranslation();
-  const currentLanguage = i18n.language;
-
-  // Alle ondersteunde talen
-  const languages = ['nl', 'en', 'de', 'fr', 'es', 'it', 'pl', 'pt', 'sv', 'da'];
+  const { t } = useTranslation();
 
   return (
     <Helmet>
-      {/* Basis meta tags */}
-      <html lang={currentLanguage} />
-      <title>{t('meta.title')}</title>
-      <meta name="description" content={t('meta.description')} />
-
-      {/* Open Graph tags */}
-      <meta property="og:title" content={t('meta.title')} />
-      <meta property="og:description" content={t('meta.description')} />
+      <title>{t('metadata.title')}</title>
+      <meta name="description" content={t('metadata.description')} />
+      
+      {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={window.location.href} />
-      <meta property="og:image" content="/images/og-image.png" />
-      <meta property="og:locale" content={currentLanguage} />
-
-      {/* Alternate language versions */}
-      {languages.map(lang => (
-        <link
-          key={lang}
-          rel="alternate"
-          hrefLang={lang}
-          href={`${window.location.origin}/${lang === 'en' ? '' : lang}`}
-        />
-      ))}
-
-      {/* Schema.org markup */}
-      <script type="application/ld+json">
-        {JSON.stringify({
-          '@context': 'https://schema.org',
-          '@type': 'Organization',
-          'name': 'SkillVal',
-          'url': window.location.origin,
-          'logo': `${window.location.origin}/images/logo-dark.png`,
-          'description': t('meta.description'),
-          'sameAs': [
-            'https://www.linkedin.com/company/skillval',
-            'https://twitter.com/skillval_eu'
-          ]
-        })}
-      </script>
+      <meta property="og:url" content="https://viberise.me/" />
+      <meta property="og:title" content={t('metadata.title')} />
+      <meta property="og:description" content={t('metadata.description')} />
+      
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://viberise.me/" />
+      <meta property="twitter:title" content={t('metadata.title')} />
+      <meta property="twitter:description" content={t('metadata.description')} />
     </Helmet>
   );
 };
