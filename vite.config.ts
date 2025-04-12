f4bc6e96e4a32a86ee5e3e8541bdde8a68822099
Voskesss <josklijnhout@hotmail.com>
2025-04-12 17:33:29 +0200

    deploy 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/viberisemewebsite/',  // Terug naar de GitHub Pages path
+  base: '/viberisemewebsite/',  // Voor GitHub Pages
   build: {
     outDir: 'docs',
     emptyOutDir: true
